36666b0107027e4702e9df1ea65d560be143f89b
Ubuntu <ubuntu@ip-172-31-33-54.us-east-2.compute.internal>
2021-09-24 12:18:25 +0000

    Finish Emp_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  resources :microposts
+  resources :employees
+  root 'employees#index'
+  #root 'microposts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210924113532_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :Name
+      t.integer :Age
+      t.string :Gender
+      t.string :Designation
+      t.string :Email
+
+      t.timestamps
+    end
+  end
+end
